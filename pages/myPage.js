2c200ba018b1f97a1d5ce3a2bec1fd6354cc6c90
praconfi <praconfi@gmail.com>
2020-09-09 11:01:34 +0900

    🔧fix plantList page

--- pages/myPage.js
+++ pages/myPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-<<<<<<< HEAD
 import styles from "../styles/myPage.module.css";
 import { Modal, Button, Form } from "react-bootstrap";
 import { observer, useObserver, useLocalStore } from 'mobx-react'
@@ -9,234 +8,58 @@ import { observable } from "mobx";
 
 const myPage = () => {
   const state = useLocalStore(() => ({
-    email : '',
-    password : '',
-    username : '',
-  onChangeEmail(e) {
+    email: '',
+    password: '',
+    username: '',
+    onChangeEmail(e) {
       this.email = e.target.value;
-  },
-  onChangePassword(e) {
-      this.password =  e.target.value;
-  },
-  onChangeUsername(e) {
-      this.username =  e.target.value;
-  }
+    },
+    onChangePassword(e) {
+      this.password = e.target.value;
+    },
+    onChangeUsername(e) {
+      this.username = e.target.value;
+    }
   }));
-  return useObserver (() => { 
-    return(
-    <div className={styles.flex_container}>
-      <div className={styles.flex_item1}>
-        <div>
-          내가 쓴 글
+  return useObserver(() => {
+    return (
+      <div className={styles.flex_container}>
+        <div className={styles.flex_item1}>
+          <div>
+            내가 쓴 글
           <div>2222222</div>
-        </div>
-        <div>
-          내가 쓴 댓글
+          </div>
+          <div>
+            내가 쓴 댓글
           <div>3333333</div>
+          </div>
         </div>
-      </div>
-      <div className={styles.flex_item2}>
-        <Form className={styles.form_css}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>이름</Form.Label>
-            <Form.Label type="username">{userState.data}</Form.Label>
-          </Form.Group>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>이메일</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-           <Form.Label>비밀번호</Form.Label>
-           <Form.Control type="password" placeholder="Password"/>
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>비밀번호 확인</Form.Label>
-            <Form.Control type="password" placeholder="Password"/>
-          </Form.Group>
-          <Button variant="primary">
+        <div className={styles.flex_item2}>
+          <Form className={styles.form_css}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>이름</Form.Label>
+              <Form.Label type="username">{userState.data}</Form.Label>
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>이메일</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>비밀번호</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>비밀번호 확인</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary">
               변경하기
           </Button>
-        </Form>
+          </Form>
+        </div>
       </div>
-    </div>
     )
   })
-=======
-
-const Line = styled.line`
-  position: absolute;
-  width: 737.5px;
-  height: 0px;
-  left: calc(50% - 737.5px / 2 + 369.25px);
-  top: calc(50% - 0px / 2 - 322px);
-  border: 1px solid #c4c4c4;
-  transform: rotate(90deg);
-`;
-const HistoryTemp = styled.form`
-  position: absolute;
-  width: 750px;
-  height: 269px;
-  left: 79px;
-  top: 220px;
-
-  background: #ffffff;
-  border: 1px solid #000000;
-  box-sizing: border-box;
-`;
-const History1 = styled.form`
-  position: absolute;
-  width: 715px;
-  height: 47px;
-  background: #c4c4c4;
-`;
-const History2 = styled.form`
-  position: absolute;
-  width: 715px;
-  height: 47px;
-  left: 96px;
-  top: 298px;
-
-  background: #c4c4c4;
-`;
-const History3 = styled.form`
-  position: absolute;
-  width: 715px;
-  height: 47px;
-  left: 96px;
-  top: 355px;
-
-  background: #c4c4c4;
-`;
-const History4 = styled.form`
-  position: absolute;
-  width: 715px;
-  height: 47px;
-  left: 96px;
-  top: 412px;
-
-  background: #c4c4c4;
-`;
-
-const HistoryRepTemp = styled.form`
-  position: absolute;
-  width: 750px;
-  height: 269px;
-
-  background: #ffffff;
-  border: 1px solid #000000;
-  box-sizing: border-box;
-`;
-const HistoryRep1 = styled.form`
-  position: absolute;
-  width: 715px;
-  height: 47px;
-
-  background: #c4c4c4;
-`;
-const HistoryRep2 = styled.form`
-  position: absolute;
-  width: 715px;
-  height: 47px;
-
-  background: #c4c4c4;
-`;
-const HistoryRep3 = styled.form`
-  position: absolute;
-  width: 715px;
-  height: 47px;
-
-  background: #c4c4c4;
-`;
-const HistoryRep4 = styled.form`
-  position: absolute;
-  width: 715px;
-  height: 47px;
-
-  background: #c4c4c4;
-`;
-const ID = styled.input`
-  position: absolute;
-  width: 515px;
-  height: 77px;
-  left: 1213px;
-  top: 262px;
-  background: #ffffff;
-  border: 1px solid #000000;
-  box-sizing: border-box;
-`;
-const Email = styled.input`
-  position: absolute;
-  width: 515px;
-  height: 75px;
-  left: 1213px;
-  top: 383px;
-
-  background: #ffffff;
-  border: 1px solid #000000;
-  box-sizing: border-box;
-`;
-const PW = styled.input`
-  position: absolute;
-  width: 515px;
-  height: 77px;
-  left: 1213px;
-  top: 500px;
-
-  background: #ffffff;
-  border: 1px solid #000000;
-  box-sizing: border-box;
-`;
-const PW2 = styled.input`
-  position: absolute;
-  width: 515px;
-  height: 74px;
-  left: 1213px;
-  top: 621px;
-
-  background: #ffffff;
-  border: 1px solid #000000;
-  box-sizing: border-box;
-`;
-const Confirm = styled.input`
-  position: absolute;
-  width: 142px;
-  height: 73px;
-  left: 1586px;
-  top: 763px;
-
-  background: #ffffff;
-  border: 1px solid #000000;
-  box-sizing: border-box;
-`;
-
-const myPage = () => {
-  return (
-    <div>
-      <div>
-        <HistoryTemp>
-          <History1></History1>
-          <History2></History2>
-          <History3></History3>
-          <History4></History4>
-        </HistoryTemp>
-        <HistoryRepTemp>
-          <HistoryRep1></HistoryRep1>
-          <HistoryRep2></HistoryRep2>
-          <HistoryRep3></HistoryRep3>
-          <HistoryRep4></HistoryRep4>
-        </HistoryRepTemp>
-      </div>
-      <Line></Line>
-      <div>
-        <ID></ID>
-        <Email></Email>
-        <PW></PW>
-        <PW2></PW2>
-        <Confirm></Confirm>
-      </div>
-    </div>
-  );
->>>>>>> b1a6840a539c1c06e1e0640f0c87f62b5653540a
 };
 
 export default myPage;
--- pages/plantList.js
+++ pages/plantList.js
@@ -1,105 +1,10 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import Link from "next/link";
 import Nav from '../components/nav'
-import { plantListStore, photoStore } from '../store/plantList.js'
 import axios from 'axios';
 import styled from "styled-components";
-import { useObserver, useLocalStore } from 'mobx-react'
-import { runInAction, action } from 'mobx';
-
-const expectedRes = [
-    {
-        "id": 3,
-        "title": "title is...",
-        "content": "content is...",
-        "level": "normal",
-        "season": "summer",
-        "category": "flower",
-        "photoUrl": "[\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293730105.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293731266.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293732322.jpg\"]",
-        "userId": 1,
-        "createdAt": "2020-09-05T08:15:34.000Z",
-        "updatedAt": "2020-09-05T08:15:34.000Z",
-        "UserId": 1
-    },
-    {
-        "id": 4,
-        "title": "title is...",
-        "content": "content is...",
-        "level": "normal",
-        "season": "summer",
-        "category": "flower",
-        "photoUrl": "[\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293761946.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293763071.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293767487.jpg\"]",
-        "userId": 1,
-        "createdAt": "2020-09-05T08:16:09.000Z",
-        "updatedAt": "2020-09-05T08:16:09.000Z",
-        "UserId": 1
-    },
-    {
-        "id": 5,
-        "title": "title is...",
-        "content": "content is...",
-        "level": "normal",
-        "season": "summer",
-        "category": "flower",
-        "photoUrl": "[\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293791007.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293792174.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293793449.jpg\"]",
-        "userId": 1,
-        "createdAt": "2020-09-05T08:16:35.000Z",
-        "updatedAt": "2020-09-05T08:16:35.000Z",
-        "UserId": 1
-    },
-    {
-        "id": 6,
-        "title": "title is...",
-        "content": "content is...",
-        "level": "normal",
-        "season": "summer",
-        "category": "flower",
-        "photoUrl": "[\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293796707.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293797696.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293798592.jpg\"]",
-        "userId": 1,
-        "createdAt": "2020-09-05T08:16:40.000Z",
-        "updatedAt": "2020-09-05T08:16:40.000Z",
-        "UserId": 1
-    },
-    {
-        "id": 7,
-        "title": "title is...",
-        "content": "content is...",
-        "level": "normal",
-        "season": "summer",
-        "category": "flower",
-        "photoUrl": "[\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293796707.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293797696.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293798592.jpg\"]",
-        "userId": 1,
-        "createdAt": "2020-09-05T08:16:40.000Z",
-        "updatedAt": "2020-09-05T08:16:40.000Z",
-        "UserId": 1
-    },
-    {
-        "id": 8,
-        "title": "title is...",
-        "content": "content is...",
-        "level": "normal",
-        "season": "summer",
-        "category": "flower",
-        "photoUrl": "[\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293796707.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293797696.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293798592.jpg\"]",
-        "userId": 1,
-        "createdAt": "2020-09-05T08:16:40.000Z",
-        "updatedAt": "2020-09-05T08:16:40.000Z",
-        "UserId": 1
-    },
-    {
-        "id": 9,
-        "title": "title is...",
-        "content": "content is...",
-        "level": "normal",
-        "season": "summer",
-        "category": "flower",
-        "photoUrl": "[\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293796707.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293797696.JPG\",\"https://greeneach.s3.ap-northeast-2.amazonaws.com/1599293798592.jpg\"]",
-        "userId": 1,
-        "createdAt": "2020-09-05T08:16:40.000Z",
-        "updatedAt": "2020-09-05T08:16:40.000Z",
-        "UserId": 1
-    }
-]
+import styles from '../styles/plantList.module.css';
+import { useObserver } from 'mobx-react'
 
 const state = {
     level: 'normal',
@@ -109,121 +14,39 @@ const state = {
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InByYWNvbmZpQGdtYWlsLmNvbSIsImlkIjozLCJpYXQiOjE1OTkzOTU3NDJ9.RviXSvWRNx_52lkLyZXnBCYl8LTylpGSIRKxcXSSL9Y"
 
 const PlantList = () => {
-
+    const [lists, setLists] = useState([])
     const SpecificContent = async () => {
         const result = await axios.post('http://18.191.16.175:3000/content/specific', { ...state }, { headers: { token: token } })
-        console.log('result', result.data);
-        plantListStore.listData = result.data
-        await action(() => {
-            plantListStore.listData = result.data
-        })
-    }
+        setLists(result.data)
 
+    }
     useEffect(() => {
         console.log('useEffectTest');
         SpecificContent()
     }, [])
 
-    console.log(plantListStore.listData)
-    const mappedImg = expectedRes.map((list, id) =>
-        <Link href='plant' key={id}>
-            <div className="list" >
-                <img className="list__photo" width="250px" height="250px" src={JSON.parse(list.photoUrl)[0]}   ></img >
-                <div className="list__description" >
-                    <h3 >{list.title}</h3>
-                    <span >{list.content}</span>
-                </div>
-            </div>
-        </Link>
-    );
-
+    console.log(lists)
     return useObserver(() => {
         return (
             <div>
-                <Nav />
-                <div className="button">
+                <div className={styles.button}>
                     <Link href="/plantAdd">
-                        <button className="create__button">Create</button>
+                        <button className={styles.create__button}>Create</button>
                     </Link>
                 </div>
-                <div className="lists">
-                    {mappedImg}
+                <div className={styles.lists}>
+                    {lists.map((list, id) =>
+                        <Link href='plant' key={id}>
+                            <div className={styles.list} >
+                                <img className={styles.list__photo} width="250px" height="250px" src={JSON.parse(list.photoUrl)[0]}   ></img >
+                                <div className={styles.list__description} >
+                                    <h3 >{list.title}</h3>
+                                    <span >{list.content}</span>
+                                </div>
+                            </div>
+                        </Link>
+                    )}
                 </div>
-                <style jsx="true">{`
-      .lists{
-      display: flex;
-      flex-wrap:wrap;
-      justify-content: center;
-      }
-      .list{
-      position: relative;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 280px;
-      height:250px;
-      margin: 4px;
-      margin-bottom: 10px;
-      cursor: pointer;
-      }
-      .list__photo{
-      max-width: 100%;
-      max-height: 100%;
-      }
-      .list__description{
-      align-items: center;
-      position:absolute ;
-      background-color: black;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      width: 100%;
-      height: 100%;
-      top: 0;
-      left: 0;
-      opacity: 0;
-      transform: translateY(10px);
-      transition: all 300ms ease-in;
-      }
-      .list:hover .list__description{
-      opacity: 0.8;
-      transform: translateY(0px);
-      }
-      .list__description h3{
-      color: white;
-      }
-      .list__description h3:after{
-      content: '';
-      display: block;
-      position: relative;
-      left: 50%;
-      margin-left: -12px;
-      margin-top: 8px;
-      width: 25px;
-      height: 2px;
-      background-color: var(--color-dark-white);
-      }
-      button{
-          background-color: transparent;
-          cursor: pointer;
-          border: none;
-          outline: none;
-          ;
-          }
-      .button{
-              text-align:center;
-              }
-      .create__button {
-              color : black;
-              font-size: 25px;
-              font-weight: 700;
-              margin : 34px;
-              padding : 15px 20px;
-              border: 3px solid grey;
-              border-radius: 10px;
-              }
-            `}
-                </style>
             </div>
         )
     })
