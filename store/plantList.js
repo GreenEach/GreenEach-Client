fcc86eba501c3076fd997ef189ddd592817f97f1
praconfi <praconfi@gmail.com>
2020-09-11 15:44:36 +0900

    🔧fix #22, #34,

--- store/plantList.js
+++ store/plantList.js
@@ -1,7 +1,16 @@
 const { observable } = require("mobx");
 
 const plantListStore = observable({
-    listData: ''
+    listId: '',
+
+    setId(id) {
+        this.listId = id;
+    },
+
+    commentList: [],
+    setCommentList(array) {
+        this.commentList = array;
+    }
 })
 
 export { plantListStore };
