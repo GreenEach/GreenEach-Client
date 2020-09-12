55f3732087480b3c8caad5daad6fc76e8ea5529f
hanur92 <rkdgksdjfwkd92@gmail.com>
2020-09-12 22:41:36 +0900

    deploy

--- pages/plant.js
+++ pages/plant.js
@@ -30,7 +30,7 @@ const Plant = ({ cookies }) => {
   let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   let jsonPayload = decodeURIComponent(
     atob(base64)
-      .split("")
+      // .split("")
       .map(function (c) {
         return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
       })
