7e54ddfe9ac30b9a30da93ac989e4d05bf4cdabf
Jundev <junhyeong3289@gmail.com>
2020-09-19 10:46:09 +0900

    ➕merge commit

--- pages/index.js
+++ pages/index.js
@@ -48,25 +48,24 @@ const Home = ({ cookies }) => {
             </p>
           </Link>
         </div>
-        <div className={styles.flex_item2}>
+        <div className={styles.intro_Container}>
           <p className={styles.benefit}> GreenEach Introduce</p>
           <div className={styles.flex_item2_container}>
-            <div className={styles.flex_item2_item}>
-              <img src="/login.png" className={styles.icon}></img>
+            <div>
+              <div className={styles.flex_introItem}></div>
             </div>
-            <div className={styles.flex_item2_item}>
-              <img src="/social.png" className={styles.icon}></img>
+            <div>
+              <div className={styles.flex_introItem}></div>
             </div>
-            <div className={styles.flex_item2_item}>
-              <img src="/board.png" className={styles.icon}></img>
+            <div>
+              <div className={styles.flex_introItem}></div>
             </div>
-          </div>
-          <div className={styles.flex_item2_container2}>
-            <div className={styles.child1}>로그인 후 사용</div>
-            <div className={styles.child2}>다양한 유저들과 소통</div>
-            <div className={styles.child3}>각종 게시글 및 데이터 활용</div>
-          </div>
+            <div>
+              <div className={styles.flex_introItem}></div>
+            </div>
+           </div>
         </div>
+        
         <div className={styles.flex_item2}>
           <p className={styles.benefit}> GreenEach Benefit</p>
           <div className={styles.flex_item2_container}>
