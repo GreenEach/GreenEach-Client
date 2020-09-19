71fa2e929f2a7295560c9782bbd76acab7f0858d
hanur92 <rkdgksdjfwkd92@gmail.com>
2020-09-19 22:10:12 +0900

    🔧 fix myPage

--- pages/myPage.js
+++ pages/myPage.js
@@ -7,10 +7,10 @@ import { Cookies, withCookies } from "react-cookie";
 
 const myPage = ({ cookies }) => {
   const state = useLocalStore(() => ({
-    email: '',
-    password: '',
-    password2: '',
-    username: '',
+    email: "",
+    password: "",
+    password2: "",
+    username: "",
     contents: [],
     comments: [],
     onChangeUserName(e) {
@@ -24,112 +24,97 @@ const myPage = ({ cookies }) => {
     },
   }));
 
-  let base64Url = cookies.get("userInfo").split('.')[1];
-  let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
+  state.email = JSON.parse(jsonPayload).email;
 
-  state.email = JSON.parse(jsonPayload).email
-
-  
-
-  const getCommentsContents = () =>{
-    Axios.post('http://18.191.16.175:3000/sign/mypage',
+  const getCommentsContents = () => {
+    Axios.post(
+      "http://18.191.16.175:3000/sign/mypage",
       {},
-      { headers: { token: cookies.get("userInfo") } },
+      { headers: { token: cookies.get("userInfo") } }
     )
       .then((response) => {
-        let contentsArr = []
-        let commentsArr = []
-        console.log("data.response = ",response.data[0])
-       //업뎃 시간, 카테고리(레벨, 시즌) [[title, ]]
+        let contentsArr = [];
+        let commentsArr = [];
+        console.log("data.response = ", response.data[0]);
+        //업뎃 시간, 카테고리(레벨, 시즌) [[title, ]]
         for (let i = 0; i < response.data[0].Contents.length; i++) {
-          contentsArr.push(
-            [response.data[0].Contents[i].title,
+          contentsArr.push([
+            response.data[0].Contents[i].title,
             response.data[0].Contents[i].level,
             response.data[0].Contents[i].season,
-            response.data[0].Contents[i].updatedAt]
-          ); 
+            response.data[0].Contents[i].updatedAt,
+          ]);
         }
 
-        for(let j = 0; j < response.data[0].Comments.length; j++){
-          commentsArr.push(
-            [response.data[0].Comments[j].comment,
-            response.data[0].Comments[j].updatedAt]
-            
-           );
+        for (let j = 0; j < response.data[0].Comments.length; j++) {
+          commentsArr.push([
+            response.data[0].Comments[j].comment,
+            response.data[0].Comments[j].updatedAt,
+          ]);
         }
 
         state.contents = contentsArr;
         state.comments = commentsArr;
-        
       })
       .catch((err) => {
-        console.log(err)
-      })
-        
-  }
+        console.log(err);
+      });
+  };
 
-    useEffect(() => {
-      getCommentsContents()
-    }, [])
+  useEffect(() => {
+    getCommentsContents();
+  }, []);
 
- 
-  const contentsList = () =>{
-   return state.contents.map(
-      (data, i) => {
-        let yearMonthDay = data[3].substring(0, 10);
-        let hour = data[3].substring(11, 13);
-        let min = data[3].substring(14, 16);
-        return(
-          <tbody>
+  const contentsList = () => {
+    return state.contents.map((data, i) => {
+      let yearMonthDay = data[3].substring(0, 10);
+      let hour = data[3].substring(11, 13);
+      let min = data[3].substring(14, 16);
+      return (
+        <tbody>
           <tr>
-          {console.log(data)}
-            <td>{i+1}</td>
+            {console.log(data)}
+            <td>{i + 1}</td>
             <td>{data[0]}</td>
             <td>{data[1]}</td>
             <td>{data[2]}</td>
-            <td> {yearMonthDay}일 {"" + hour}시 {"" + min}분</td>
+            <td>
+              {" "}
+              {yearMonthDay}일 {"" + hour}시 {"" + min}분
+            </td>
           </tr>
         </tbody>
-        )  
-      }
-       
-    )
-  }
+      );
+    });
+  };
 
   useEffect(() => {
-    contentsList()
-  }, [])
+    contentsList();
+  }, []);
 
-    const commentsList = () =>{
-      return state.comments.map(
-          (data, i) => {
-            let yearMonthDay = data[1].substring(0, 10);
-            let hour = data[1].substring(11, 13);
-            let min = data[1].substring(14, 16);
-            return(
-              <tbody>
-              <tr>
-                <td>{i+1}</td>
-                <td>{data[0]}</td>
-                <td> {yearMonthDay}일 {"" + hour}시 {"" + min}분</td>
-              </tr>
-            </tbody>
-            )
-             
-        
-          
-          }
-            )
-        
-    }
+  const commentsList = () => {
+    return state.comments.map((data, i) => {
+      let yearMonthDay = data[1].substring(0, 10);
+      let hour = data[1].substring(11, 13);
+      let min = data[1].substring(14, 16);
+      return (
+        <tbody>
+          <tr>
+            <td>{i + 1}</td>
+            <td>{data[0]}</td>
+            <td>
+              {" "}
+              {yearMonthDay}일 {"" + hour}시 {"" + min}분
+            </td>
+          </tr>
+        </tbody>
+      );
+    });
+  };
 
-    useEffect(() => {
-      commentsList()
-    }, [])
-  
+  useEffect(() => {
+    commentsList();
+  }, []);
 
   return useObserver(() => {
     return (
@@ -139,10 +124,10 @@ const myPage = ({ cookies }) => {
            <div className={styles.userInfoEmail}>email : {state.email}</div>
           
         </div> */}
-         <div className={styles.flex_item1}></div>
+        <div className={styles.flex_item1}></div>
         <div className={styles.contentsContainer}>
-            <div className={styles.title}>내가 쓴 글</div>
-            <div className={styles.item1_contents}>
+          <div className={styles.title}>내가 쓴 글</div>
+          <div className={styles.item1_contents}>
             <Table>
               <thead>
                 <tr>
@@ -155,23 +140,23 @@ const myPage = ({ cookies }) => {
               </thead>
               {contentsList()}
             </Table>
-            </div>
           </div>
+        </div>
 
         <div className={styles.flex_item2}>
           <div className={styles.commentsContainer}>
             <div className={styles.title}>내가 쓴 댓글</div>
             <div className={styles.item1_comments}>
-            <Table>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Comments</th>
-                  <th>Update</th>
-                </tr>
-              </thead>
-              {commentsList()}
-            </Table>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Comments</th>
+                    <th>Update</th>
+                  </tr>
+                </thead>
+                {commentsList()}
+              </Table>
             </div>
           </div>
         </div>
